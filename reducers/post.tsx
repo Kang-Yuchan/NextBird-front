aa7a9ffa08cc3fdeebbe99bdd0f3a1b13cdf67b0
Yuchan <mikuplayer123@gmail.com>
2020-06-07 02:46:01 +0900

    First Reducer

---
+++ reducers/post.tsx
@@ -0,0 +1,44 @@
+export const initialState = {
+  mainPosts: [],
+};
+
+const ADD_POST = 'ADD_POST';
+const ADD_DUMMY = 'ADD_DUMMY';
+
+export const addPost = {
+  type: ADD_POST,
+  data: {
+    isLoggedIn: false,
+  },
+};
+
+export const addDummy = {
+  type: ADD_DUMMY,
+  data: {
+    content: 'Hello!!~',
+    UserId: 1,
+    User: {
+      name: 'yuchan',
+    },
+  },
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST: {
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: action.data,
+      };
+    }
+    case ADD_DUMMY: {
+      return {
+        ...state,
+        mainPosts: [action.data, ...state.mainPosts],
+      };
+    }
+  }
+};
+
+export default reducer;
---
+++ reducers/user.tsx
@@ -0,0 +1,38 @@
+export const initialState = {
+  isLoggedIn: false,
+  user: {},
+};
+
+const LOG_IN = 'LOG_IN';
+const LOG_OUT = 'LOG_OUT';
+
+export const loginAction = {
+  type: LOG_IN,
+  data: {
+    name: 'yuchan',
+  },
+};
+export const logoutAction = {
+  type: LOG_OUT,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOG_IN: {
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: action.data,
+      };
+    }
+    case LOG_OUT: {
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+      };
+    }
+  }
+};
+
+export default reducer;
