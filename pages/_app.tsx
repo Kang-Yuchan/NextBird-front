9cff69dbe514e595cf84bd678974d68475b6c78d
Yuchan <mikuplayer123@gmail.com>
2020-06-13 20:36:06 +0900

    Connect saga to redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,10 @@ import AppLayout from '../components/AppLayout';
 import { ComponentProps } from '../interface';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware, StoreEnhancer } from 'redux';
 import reducer from '../reducers';
+import createSagaMiddleware, { SagaMiddleware } from 'redux-saga';
+import rootSaga from '../sagas';
 
 const NextBird = ({ Component, pageProps, store }: ComponentProps): React.ReactNode => {
   return (
@@ -21,14 +23,21 @@ const NextBird = ({ Component, pageProps, store }: ComponentProps): React.ReactN
   );
 };
 
-export default withRedux((initialState, options) => {
-  const middlewares: Array<any> = [];
-  const enhancer = compose(
-    applyMiddleware(...middlewares),
-    !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== 'undefined'
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f,
-  );
+const configureStore = (initialState, options) => {
+  const sagaMiddleware: SagaMiddleware = createSagaMiddleware();
+  const middlewares: Array<any> = [sagaMiddleware];
+  const enhancer: StoreEnhancer =
+    process.env.NODE_ENV === 'production'
+      ? compose(applyMiddleware(...middlewares))
+      : compose(
+          applyMiddleware(...middlewares),
+          !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== 'undefined'
+            ? window.__REDUX_DEVTOOLS_EXTENSION__()
+            : (f) => f,
+        );
   const store = createStore(reducer, initialState, enhancer);
+  sagaMiddleware.run(rootSaga);
   return store;
-})(NextBird);
+};
+
+export default withRedux(configureStore)(NextBird);
