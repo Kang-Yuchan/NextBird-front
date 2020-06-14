417c0a4231de21f39eae578a716621d740465cc5
Yuchan <mikuplayer123@gmail.com>
2020-06-14 21:27:11 +0900

    Loop in Saga

--- sagas/user.tsx
+++ sagas/user.tsx
@@ -1,4 +1,4 @@
-import { all, fork, takeLatest, call, put } from 'redux-saga/effects';
+import { all, fork, take, call, put, delay } from 'redux-saga/effects';
 import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from '../reducers/user';
 
 function loginAPI() {
@@ -20,7 +20,13 @@ function* login(): Generator {
 }
 
 function* watchLogin(): Generator {
-  yield takeLatest(LOG_IN, login);
+  while (true) {
+    yield take(LOG_IN);
+    yield delay(1000);
+    yield put({
+      type: LOG_IN_SUCCESS,
+    });
+  }
 }
 
 export default function* userSaga(): Generator {
