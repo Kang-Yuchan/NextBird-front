4c68d92e50869b89588767fb11727aeab6a6e039
Yuchan <mikuplayer123@gmail.com>
2020-06-15 21:59:57 +0900

    Redux state & actions structure

--- sagas/user.tsx
+++ sagas/user.tsx
@@ -1,7 +1,25 @@
 import { all, fork, takeLatest, call, put } from 'redux-saga/effects';
-import { LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_IN_FAILURE } from '../reducers/user';
+import {
+  LOG_IN_REQUEST,
+  LOG_IN_SUCCESS,
+  LOG_IN_FAILURE,
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_FAILURE,
+  LOG_OUT_REQUEST,
+  LOG_OUT_SUCCESS,
+  LOG_OUT_FAILURE,
+} from '../reducers/user';
 
 function loginAPI() {
+  //request to server
+}
+
+function logoutAPI() {
+  //request to server
+}
+
+function signUpAPI() {
   //request to server
 }
 
@@ -19,10 +37,46 @@ function* login(): Generator {
   }
 }
 
+function* logout(): Generator {
+  try {
+    yield call(logoutAPI);
+    yield put({
+      type: LOG_OUT_SUCCESS,
+    });
+  } catch (error) {
+    console.error(error);
+    yield put({
+      type: LOG_OUT_FAILURE,
+    });
+  }
+}
+
+function* signUp(): Generator {
+  try {
+    yield call(signUpAPI);
+    yield put({
+      type: SIGN_UP_SUCCESS,
+    });
+  } catch (error) {
+    console.error(error);
+    yield put({
+      type: SIGN_UP_FAILURE,
+    });
+  }
+}
+
 function* watchLogin(): Generator {
   yield takeLatest(LOG_IN_REQUEST, login);
 }
 
+function* watchLogout(): Generator {
+  yield takeLatest(LOG_OUT_REQUEST, logout);
+}
+
+function* watchSignup(): Generator {
+  yield takeLatest(SIGN_UP_REQUEST, signUp);
+}
+
 export default function* userSaga(): Generator {
-  yield all([fork(watchLogin)]);
+  yield all([fork(watchLogin), fork(watchLogout), fork(watchSignup)]);
 }
