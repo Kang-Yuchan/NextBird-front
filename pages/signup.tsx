9457c751616fa63475ea9df8bf7a95c0feae45d9
Yuchan <mikuplayer123@gmail.com>
2020-06-20 21:32:01 +0900

    Real sign up

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -5,131 +5,122 @@ import { Input, Checkbox, Button } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { SIGN_UP_REQUEST } from '../reducers/user';
 
-export const Form = styled.form`
-  padding: 10;
-`;
+export const Form = styled.form`padding: 10;`;
 
-const ErrorMsg = styled.span`
-  color: red;
-`;
+const ErrorMsg = styled.span`color: red;`;
 
-const SignupDiv = styled.div`
-  margin-top: 10px;
-`;
+const SignupDiv = styled.div`margin-top: 10px;`;
 
 export type UseInputType = [string, (e: React.FormEvent<EventTarget>) => void];
 
 export const useInput = (initialValue: string): UseInputType => {
-  const [value, setter] = React.useState<string>(initialValue);
-  const handler = React.useCallback((e: React.FormEvent<EventTarget>): void => {
-    const target = e.target as HTMLInputElement;
-    setter(target.value);
-  }, []);
-  return [value, handler];
+	const [ value, setter ] = React.useState<string>(initialValue);
+	const handler = React.useCallback((e: React.FormEvent<EventTarget>): void => {
+		const target = e.target as HTMLInputElement;
+		setter(target.value);
+	}, []);
+	return [ value, handler ];
 };
 
 const Signup: React.FunctionComponent = () => {
-  const [id, onChangeId]: UseInputType = useInput('');
-  const [password, onChangePassword]: UseInputType = useInput('');
-  const [passwordCheck, setPasswordCheck] = React.useState<string>('');
-  const [term, setTerm] = React.useState<boolean>(false);
-  const [passwordError, setPasswordError] = React.useState<boolean>(false);
-  const [termError, setTermError] = React.useState<boolean>(false);
-  const dispatch = useDispatch();
-  const { isSigningUp, me } = useSelector((state) => state.user);
+	const [ id, onChangeId ]: UseInputType = useInput('');
+	const [ password, onChangePassword ]: UseInputType = useInput('');
+	const [ passwordCheck, setPasswordCheck ] = React.useState<string>('');
+	const [ term, setTerm ] = React.useState<boolean>(false);
+	const [ passwordError, setPasswordError ] = React.useState<boolean>(false);
+	const [ termError, setTermError ] = React.useState<boolean>(false);
+	const dispatch = useDispatch();
+	const { isSigningUp, me } = useSelector((state) => state.user);
 
-  React.useEffect(() => {
-    if (me) {
-      alert('Go to main page because you are logged in.');
-      Router.push('/');
-    }
-  }, [me && me.id]);
+	React.useEffect(
+		() => {
+			if (me) {
+				alert('Go to main page because you are logged in.');
+				Router.push('/');
+			}
+		},
+		[ me && me.id ]
+	);
 
-  const onSubmit = React.useCallback(
-    (e: React.FormEvent<EventTarget>): void => {
-      e.preventDefault();
-      if (!password) {
-        setPasswordError(true);
-      }
+	const onSubmit = React.useCallback(
+		(e: React.FormEvent<EventTarget>): void => {
+			e.preventDefault();
+			if (!password) {
+				setPasswordError(true);
+			}
 
-      if (password !== passwordCheck) {
-        setPasswordError(true);
-      }
+			if (password !== passwordCheck) {
+				setPasswordError(true);
+			}
 
-      if (!term) {
-        setTermError(true);
-      }
-      dispatch({
-        type: SIGN_UP_REQUEST,
-        data: {
-          id,
-          password,
-        },
-      });
-    },
-    [password, passwordCheck, term],
-  );
+			if (!term) {
+				setTermError(true);
+			}
+			dispatch({
+				type: SIGN_UP_REQUEST,
+				data: {
+					userId: id,
+					password
+				}
+			});
+		},
+		[ id, password, passwordCheck, term ]
+	);
 
-  const onChangePasswordCheck = React.useCallback(
-    (e: React.FormEvent<EventTarget>): void => {
-      const target = e.target as HTMLInputElement;
-      setPasswordError(target.value !== password);
-      setPasswordCheck(target.value);
-    },
-    [password],
-  );
+	const onChangePasswordCheck = React.useCallback(
+		(e: React.FormEvent<EventTarget>): void => {
+			const target = e.target as HTMLInputElement;
+			setPasswordError(target.value !== password);
+			setPasswordCheck(target.value);
+		},
+		[ password ]
+	);
 
-  const onChangeTerm = React.useCallback((e: React.ChangeEvent): void => {
-    const target = e.target as HTMLInputElement;
-    setTermError(!target.checked);
-    setTerm(target.checked);
-  }, []);
+	const onChangeTerm = React.useCallback((e: React.ChangeEvent): void => {
+		const target = e.target as HTMLInputElement;
+		setTermError(!target.checked);
+		setTerm(target.checked);
+	}, []);
 
-  return (
-    <React.Fragment>
-      <Form onSubmit={onSubmit}>
-        <div>
-          <label htmlFor="user-id">ID</label>
-          <br />
-          <Input name="user-id" required onChange={onChangeId} value={id} />
-        </div>
-        <div>
-          <label htmlFor="user-password">Password</label>
-          <br />
-          <Input
-            type="password"
-            name="user-password"
-            required
-            onChange={onChangePassword}
-            value={password}
-          />
-        </div>
-        {passwordError && <ErrorMsg>This password is incorrect.</ErrorMsg>}
-        <div>
-          <label htmlFor="user-password-check">Password Check</label>
-          <br />
-          <Input
-            type="password"
-            name="user-password-check"
-            onChange={onChangePasswordCheck}
-            value={passwordCheck}
-          />
-          {passwordError && <ErrorMsg>This password is incorrect</ErrorMsg>}
-        </div>
-        <div>
-          <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
-            Please repeat that in a respectful way
-          </Checkbox>
-          {termError && <ErrorMsg>Please check the term.</ErrorMsg>}
-        </div>
-        <SignupDiv>
-          <Button type="primary" htmlType="submit" loading={isSigningUp}>
-            Submit
-          </Button>
-        </SignupDiv>
-      </Form>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Form onSubmit={onSubmit}>
+				<div>
+					<label htmlFor="user-id">ID</label>
+					<br />
+					<Input name="user-id" required onChange={onChangeId} value={id} />
+				</div>
+				<div>
+					<label htmlFor="user-password">Password</label>
+					<br />
+					<Input type="password" name="user-password" required onChange={onChangePassword} value={password} />
+				</div>
+				{passwordError && <ErrorMsg>This password is incorrect.</ErrorMsg>}
+				<div>
+					<label htmlFor="user-password-check">Password Check</label>
+					<br />
+					<Input
+						type="password"
+						name="user-password-check"
+						onChange={onChangePasswordCheck}
+						value={passwordCheck}
+					/>
+					{passwordError && <ErrorMsg>This password is incorrect</ErrorMsg>}
+				</div>
+				<div>
+					<Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+						Please repeat that in a respectful way
+					</Checkbox>
+					{termError && <ErrorMsg>Please check the term.</ErrorMsg>}
+				</div>
+				<SignupDiv>
+					<Button type="primary" htmlType="submit" loading={isSigningUp}>
+						Submit
+					</Button>
+				</SignupDiv>
+			</Form>
+		</React.Fragment>
+	);
 };
 
 export default Signup;
--- sagas/user.tsx
+++ sagas/user.tsx
@@ -1,90 +1,89 @@
 import { all, fork, takeLatest, call, put, delay } from 'redux-saga/effects';
 import Axios from 'axios';
 import {
-  LOG_IN_REQUEST,
-  LOG_IN_SUCCESS,
-  LOG_IN_FAILURE,
-  SIGN_UP_REQUEST,
-  SIGN_UP_SUCCESS,
-  SIGN_UP_FAILURE,
-  LOG_OUT_REQUEST,
-  LOG_OUT_SUCCESS,
-  LOG_OUT_FAILURE,
+	LOG_IN_REQUEST,
+	LOG_IN_SUCCESS,
+	LOG_IN_FAILURE,
+	SIGN_UP_REQUEST,
+	SIGN_UP_SUCCESS,
+	SIGN_UP_FAILURE,
+	LOG_OUT_REQUEST,
+	LOG_OUT_SUCCESS,
+	LOG_OUT_FAILURE
 } from '../reducers/user';
 
 function loginAPI() {
-  //request to server
-  return Axios.post('/login');
+	//request to server
+	return Axios.post('/login');
 }
 
 function logoutAPI() {
-  //request to server
+	//request to server
 }
 
-function signUpAPI() {
-  //request to server
+function signUpAPI(signUpData) {
+	return Axios.post('http://localhost:3065/api/user/', signUpData);
 }
 
 function* login(): Generator {
-  try {
-    //yield call(loginAPI);
-    yield delay(2000);
-    yield put({
-      type: LOG_IN_SUCCESS,
-    });
-  } catch (error) {
-    console.error(error);
-    yield put({
-      type: LOG_IN_FAILURE,
-      error: error,
-    });
-  }
+	try {
+		//yield call(loginAPI);
+		yield delay(2000);
+		yield put({
+			type: LOG_IN_SUCCESS
+		});
+	} catch (error) {
+		console.error(error);
+		yield put({
+			type: LOG_IN_FAILURE,
+			error: error
+		});
+	}
 }
 
 function* logout(): Generator {
-  try {
-    //yield call(logoutAPI);
-    yield delay(2000);
-    yield put({
-      type: LOG_OUT_SUCCESS,
-    });
-  } catch (error) {
-    console.error(error);
-    yield put({
-      type: LOG_OUT_FAILURE,
-      error: error,
-    });
-  }
+	try {
+		//yield call(logoutAPI);
+		yield delay(2000);
+		yield put({
+			type: LOG_OUT_SUCCESS
+		});
+	} catch (error) {
+		console.error(error);
+		yield put({
+			type: LOG_OUT_FAILURE,
+			error: error
+		});
+	}
 }
 
-function* signUp(): Generator {
-  try {
-    //yield call(signUpAPI);
-    yield delay(2000);
-    yield put({
-      type: SIGN_UP_SUCCESS,
-    });
-  } catch (error) {
-    console.error(error);
-    yield put({
-      type: SIGN_UP_FAILURE,
-      error: error,
-    });
-  }
+function* signUp(action): Generator {
+	try {
+		yield call(signUpAPI, action.data);
+		yield put({
+			type: SIGN_UP_SUCCESS
+		});
+	} catch (error) {
+		console.error(error);
+		yield put({
+			type: SIGN_UP_FAILURE,
+			error: error
+		});
+	}
 }
 
 function* watchLogin(): Generator {
-  yield takeLatest(LOG_IN_REQUEST, login);
+	yield takeLatest(LOG_IN_REQUEST, login);
 }
 
 function* watchLogout(): Generator {
-  yield takeLatest(LOG_OUT_REQUEST, logout);
+	yield takeLatest(LOG_OUT_REQUEST, logout);
 }
 
 function* watchSignup(): Generator {
-  yield takeLatest(SIGN_UP_REQUEST, signUp);
+	yield takeLatest(SIGN_UP_REQUEST, signUp);
 }
 
 export default function* userSaga(): Generator {
-  yield all([fork(watchLogin), fork(watchLogout), fork(watchSignup)]);
+	yield all([ fork(watchLogin), fork(watchLogout), fork(watchSignup) ]);
 }
